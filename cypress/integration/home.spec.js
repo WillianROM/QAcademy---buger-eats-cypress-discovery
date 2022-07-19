d319d2da7ebb088693b7fceeea89ea486121af9b
Willian Melo <willraf_melo@yahoo.com.br>
2022-07-18 21:29:25 -0300

    ajustando a suite home para os padrões do cypress-json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,9 @@
 
-describe("home page",()=>{
-    it("app deve estar online",()=>{ //"it" caso de teste no cypress
-        cy.viewport(1440,900)
-        cy.visit("https://buger-eats.vercel.app")
-        cy.xpath("//div[@id="page-home"]//h1").should("have.text","Seja um parceiro deliver pela Buger Eats")     //"should usado para validar
-        cy.get("#page-home main h1").should("have.text","Seja um parceiro deliver pela Buger Eats")     //"should usado para validar
+describe('home page',()=>{
+    it('app deve estar online',()=>{ //'it' caso de teste no cypress
+        // cy.viewport(1440,900)
+        cy.visit('/')
+        cy.xpath("//div[@id='page-home']//h1").should('have.text','Seja um parceiro deliver pela Buger Eats')     //'should usado para validar
+        cy.get('#page-home main h1').should('have.text','Seja um parceiro deliver pela Buger Eats')     //'should usado para validar
     }) 
 })
